36d40a180aaf3278af152dcff729e08841e77a7d
jesusgr16 <jg4663890@gmail.com>
2026-01-01 18:57:58 -0600

    Archivos nuevos

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("ventas-cache").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/app.js",
+        "/firebase.js"
+      ]);
+    })
+  );
+});
