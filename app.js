8180043a712a141dcddabc3c55dd336f4e759263
GenerateApp <jg4663890@gmail.com>
2026-02-14 20:48:53 -0600

    7.5

--- app.js
+++ app.js
@@ -308,7 +308,8 @@ async function cargarVentas() {
 // ===============================
 // PINTAR VENTA
 // ===============================
-function listaVentas.appendChild(pintarVenta(id, venta));
+listaVentas.appendChild(pintarVenta(id, venta));
+
   const li = document.createElement("li");
 
   li.innerHTML = `
@@ -577,6 +578,7 @@ window.calcResult = function () {
     }
   });
 });
+
 
 
 
